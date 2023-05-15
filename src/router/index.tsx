c14ef864b0031e16bfafede720abe1b9062f2dd0
AngryKat <gatto_candido@ukr.net>
2023-05-15 11:43:03 +0300

    Add public url to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,7 +6,7 @@ import { ErrorPage } from "./error-page";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: process.env.PUBLIC_URL + "/",
     element: <App />,
     children: [
       {
